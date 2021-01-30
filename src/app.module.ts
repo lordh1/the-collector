4598684f5904cdbfcaab293b0f0abe1d37266730
lordh <lordh@wp.pl>
2021-01-30 15:09:21 +0100

    Added authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataModule } from './data/data.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/the-collector'),
     DataModule,
+    AuthModule,
+    UsersModule,
   ],
-  controllers: [AppController],
+  controllers: [AuthController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
